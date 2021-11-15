a58fd0e500fab6f5a8f7f84703b87a1d20bc032d
gibbonp <pablo.emanuel.gibbons@gmail.com>
2021-11-15 11:21:58 -0500

    update controller text

--- src/main/java/aforo255/com/controller/TestController.java
+++ src/main/java/aforo255/com/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v1.0 | Ansible , AFORO255 - PABLO GIBBONS";
+		return "Welcome to MasterDevops |Tomcat v2.0.1 | Ansible , AFORO255 - PABLO GIBBONS";
 	}
 }
